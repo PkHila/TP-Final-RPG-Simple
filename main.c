cc7dce8ce1a24040d4c56410a287b01a7a87c314
PkHila <105527173+PkHila@users.noreply.github.com>
2022-06-07 17:31:19 -0300

    Proyecto creado
    + GitIgnore CodeBlocks

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <time.h>
+#include <string.h>
+#include "pila.h"
+
+typedef struct
+{
+    int maxVida;
+    int ataque;
+    int defensa;
+    int evasion;
+
+} Jugador;
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
